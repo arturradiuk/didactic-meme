b6818cb9d6b2b33bba40eea848cd94c0f74be81c
226452 Artur Radiuk <226452@edu.p.lodz.pl>
2021-04-02 17:26:30 +0300

    spring setup - added

---
+++ src/main/java/org/wzas/didacticmeme/DidacticMemeApplication.java
@@ -0,0 +1,13 @@
+package org.wzas.didacticmeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DidacticMemeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DidacticMemeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/wzas/didacticmeme/DidacticMemeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wzas.didacticmeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DidacticMemeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
