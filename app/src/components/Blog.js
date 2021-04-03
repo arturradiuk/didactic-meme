ff37af27332bed8ac8f840849351b11d4ba6582c
226452 Artur Radiuk <226452@edu.p.lodz.pl>
2021-04-03 17:17:13 +0300

    simple voice navigation app - added to the app folder

---
+++ app/src/components/Blog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blog = (props) => {
+    return(
+      <div style={{margin:"80px"}}>
+          <h1>Blog Page</h1>
+      </div>
+    );
+};
+
+export default Blog;
---
+++ app/src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = (props) => {
+    return(
+        <div style={{margin:"80px"}}><h1>Home Page</h1></div>
+    );
+};
+
+export default Home;
